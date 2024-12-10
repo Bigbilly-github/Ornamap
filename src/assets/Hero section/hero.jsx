2c4eea1158168f83f6d73ba94aa08035b5dad9e5
big-billy <yusufabiola555@gmail.com>
2024-12-10 19:41:35 +0100

    Hero section styling and little correction on the header and body styling

--- src/assets/Hero section/hero.jsx
+++ src/assets/Hero section/hero.jsx
@@ -2,32 +2,38 @@
 import image from '../Hero section/images/Frame 9.svg'
 import applelogo from '../Hero section/images/Group.svg'
 import googleplaylogo from '../Hero section/images/Vector.svg'
+import herostyles from '../Hero section/hero.module.css'
 function Hero (){
 
 
     return(
         <>
         <section>
-            <div>
-                <div>
-                    <div>
-                        <h1>
-                        Plan every step of your journey
-                        </h1>
-                        <p>
-                        OrnaMap app provides real-time schedules, step-by-step directions, and seamless ticketing for trains, ferries, and buses.
-                        </p>
+            <div className={herostyles.herosection}>
+                <div className={herostyles.hero}>
+                    <div className={herostyles.herobody} >
+                        <div className={herostyles.herobodydetails}>
+                            <h1>
+                            Plan every step of your journey
+                            </h1>
+                            <p>
+                            OrnaMap app provides real-time schedules, step-by-step directions, and seamless ticketing for trains, ferries, and buses.
+                            </p>
 
-                        <button >
-                            <img src={applelogo} />
-                            <img src={googleplaylogo} /> 
-                            Download App Now
-                        </button>
+                            <button >
+                                <img src={applelogo} className={herostyles.applelogo}/>
+                                <img src={googleplaylogo} className={herostyles.googleplaylogo}/> 
+                                Download App Now
+                            </button>
+
+                        </div>
+
                     </div>
 
-                </div>
-                <div>
-                    <img src={image}/>
+            
+                    <div className={herostyles.heroimage}>
+                        <img src={image}/>
+                    </div>
                 </div>
             </div>
         </section>
